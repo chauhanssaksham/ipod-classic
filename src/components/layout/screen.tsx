244a74c201c6f8343163e5ab537048c0e73a4afc
Saksham <chauhanssaksham@gmail.com>
2020-05-04 01:06:51 +0530

    Add homepage layout and top-bar styles

--- src/components/layout/screen.tsx
+++ src/components/layout/screen.tsx
@@ -1,11 +1,29 @@
 import React, {Component} from 'react'
+import BG from '../../images/screen-bg.jpg'
+import battery from '../../images/battery.png'
+import playButton from '../../images/play.png'
 
 class Screen extends Component{
     render(){
         return (
             <div className="screen">
-                <div className="screen-content">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis tenetur unde, ea accusantium recusandae veritatis dignissimos officiis et excepturi, consequatur totam tempora cupiditate perspiciatis distinctio. Assumenda veritatis, animi doloremque similique deleniti nobis alias iusto iure ad repellat necessitatibus voluptate cupiditate doloribus aspernatur sed minima hic? Autem vero totam aspernatur porro?
+                <div className="screen-content flex">
+                    <div className="home-section">
+                        <nav className="top-bar">
+                            <div className="left">
+                                iPod
+                            </div>
+                            <div className="right">
+                                <img src={battery} style={{marginTop:2, height:9, width:20}} alt="100%"/>
+                            </div>
+                            <div className="right">
+                                <img src={playButton} style={{marginTop:2, marginRight:4, height:9, width:10}} alt="100%"/>
+                            </div>
+                        </nav>
+                    </div>
+                    <div className="home-section">
+                        <img src={BG} style={{width:'100%', height:'100%'}} alt=""/>
+                    </div>
                 </div>
             </div>
         )
