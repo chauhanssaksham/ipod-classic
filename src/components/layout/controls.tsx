d20a9af32061f416772ddafac587e1bc4e4c6690
Saksham <chauhanssaksham@gmail.com>
2020-05-05 08:14:39 +0530

    Add ZingTouch sensor

--- src/components/layout/controls.tsx
+++ src/components/layout/controls.tsx
@@ -1,9 +1,42 @@
 import React, {Component} from 'react'
+import ZingTouch from 'zingtouch'
 
-class Screen extends Component{
+interface Props{
+
+}
+
+interface State{
+
+}
+
+interface Detail{
+    angle:number,
+    distanceFromOrigin: number,
+    distanceFromLast: number,
+    events: Array<any>
+}
+
+class Screen extends Component<Props, State>{
+    componentDidMount(){
+        const controlArea = document.getElementById('interactable');
+        const rotateRegion = new ZingTouch.Region(controlArea);
+        let currentAngle = 0;
+        rotateRegion.bind(controlArea, 'rotate', function(e:any){
+            const detail:Detail = e.detail;
+            currentAngle += detail.distanceFromLast;
+            if (currentAngle > 90){
+                console.log("Move down");
+                currentAngle = 0;
+            }
+            if (currentAngle < -90){
+                console.log("Move up");
+                currentAngle = 0;
+            }
+        });
+    }
     render(){
         return (
-            <div className="controls">
+            <div className="controls" id="interactable">
                 <span className="menu">MENU</span>
                 <span className="next"><i className="material-icons md-dark">fast_forward</i></span>
                 <span className="previous"><i className="material-icons">fast_rewind</i></span>
